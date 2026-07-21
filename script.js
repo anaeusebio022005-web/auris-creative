4d039f28a5849c4661a279f79eb0ff2adfcb8dce
anaeusebio022005-web <anaeusebio022005@gmail.com>
2026-07-21 22:42:34 +0100

    rascunho

--- script.js
+++
@@ -1,1 +0,0 @@
-const button=document.querySelector('.menu-button');const nav=document.querySelector('.nav');button.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));document.getElementById('year').textContent=new Date().getFullYear();
